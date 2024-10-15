bc086802acc18b9f2455016fb508db92a50403b5
Santiago Orjuela Silva <120128598+santiorjuela1@users.noreply.github.com>
2024-10-14 19:02:33 -0500

    Carrito de compras
    Carrito de compras ahora se actualiza cuando se agrega un producto

--- script.js
+++ script.js
@@ -30,3 +30,36 @@ window.onscroll = () =>{
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
+
+cart = [
+
+];
+
+const arrayCafes = [
+    {"id": 1, "nombre": "capuccino", "descripcion": "cafe con leche", "precio" : 4500},
+    {"id": 2, "nombre": "mocca", "descripcion": "moccachino" , "precio" : 5000},
+    {"id": 3, "nombre": "americano", "descripcion": "cafe sin leche", "precio" : 6000},
+    {"id": 4, "nombre": "espreso", "descripcion": "cafe con leche", "precio" : 5500},
+    {"id": 5, "nombre": "capuccino", "descripcion": "cafe con leche", "precio" : 6000},
+    {"id": 6, "nombre": "bombon", "descripcion": "cafe con leche", "precio" : 7000},
+];
+
+let total = 0;
+
+function addToCart(cafeId){
+    const cafe = arrayCafes.find(item => item.id === cafeId);
+    if(cafe){
+        cart.push(cafe)
+        console.log(cart);
+        updateCartCount();
+    }
+}
+
+
+function updateCartCount() {
+        // Get the cart-count element
+        const cartCountElement = document.getElementById('cart-count');
+        
+        // Update the text content with the length of the cart array (number of items)
+        cartCountElement.innerText = cart.length;
+    }
